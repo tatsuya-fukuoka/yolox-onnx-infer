36b983e7d30a74514cea031c0f379b0b2f60f162
Tatsuya FUKUOKA <82201896+tatsuya-fukuoka@users.noreply.github.com>
2022-12-26 22:26:33 +0900

    Update donwload_yolox_nano_onnx.sh

--- model/donwload_yolox_nano_onnx.sh
+++ model/donwload_yolox_nano_onnx.sh
@@ -1,5 +1,3 @@
-FILE_ID=17_U9mVqb6-07P2uQ-_A5fd-F2Dp7_-j-
-FILE_NAME=yolox_nano.onnx
-CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://drive.google.com/uc?export=download&id=${FILE_ID}" -O- | sed -En 's/.*confirm=([0-9A-Za-z_]+).*/\1/p');
-wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=${CONFIRM}&id=${FILE_ID}" -O ${FILE_NAME};
-rm -f /tmp/cookies.txt
+FILE_ID="17_U9mVqb6-07P2uQ-_A5fd-F2Dp7_-j-";
+FILE_NAME="yolox_nano.onnx";
+wget --no-check-certificate "http://drive.google.com/uc?export=view&id=$FILE_ID" -O $FILE_NAME;
