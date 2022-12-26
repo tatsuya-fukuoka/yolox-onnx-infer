762f7953c311f40c879f1cd7f82d419d1e8c4e98
Tatsuya FUKUOKA <82201896+tatsuya-fukuoka@users.noreply.github.com>
2022-12-26 22:27:33 +0900

    Update donwload_yolox_tiny_onnx.sh

--- model/donwload_yolox_tiny_onnx.sh
+++ model/donwload_yolox_tiny_onnx.sh
@@ -1,5 +1,3 @@
-FILE_ID=1uLZMCrYzt-bDunqO6xByPqym9bfk_5q3
-FILE_NAME=yolox_tiny.onnx
-CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://drive.google.com/uc?export=download&id=${FILE_ID}" -O- | sed -En 's/.*confirm=([0-9A-Za-z_]+).*/\1/p');
-wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=${CONFIRM}&id=${FILE_ID}" -O ${FILE_NAME};
-rm -f /tmp/cookies.txt
+FILE_ID="1uLZMCrYzt-bDunqO6xByPqym9bfk_5q3";
+FILE_NAME="yolox_tiny.onnx";
+wget --no-check-certificate "http://drive.google.com/uc?export=view&id=$FILE_ID" -O $FILE_NAME;
