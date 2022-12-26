684625c17ebc5680588736fdec23bb04ed08a504
Tatsuya FUKUOKA <82201896+tatsuya-fukuoka@users.noreply.github.com>
2022-12-26 22:27:03 +0900

    Update donwload_yolox_s_onnx.sh

--- model/donwload_yolox_s_onnx.sh
+++ model/donwload_yolox_s_onnx.sh
@@ -1,5 +1,3 @@
-FILE_ID=1kb2wgrNOp15AWYiVI70f1ll4vbvMqRqh
-FILE_NAME=yolox_s.onnx
-CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://drive.google.com/uc?export=download&id=${FILE_ID}" -O- | sed -En 's/.*confirm=([0-9A-Za-z_]+).*/\1/p');
-wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=${CONFIRM}&id=${FILE_ID}" -O ${FILE_NAME};
-rm -f /tmp/cookies.txt
+FILE_ID="1kb2wgrNOp15AWYiVI70f1ll4vbvMqRqh";
+FILE_NAME="yolox_s.onnx";
+wget --no-check-certificate "http://drive.google.com/uc?export=view&id=$FILE_ID" -O $FILE_NAME;
